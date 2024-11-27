14b609b3553ed91ad3a9892029433dd1b8a9c443
Jeff Lindsay <progrium@gmail.com>
2024-11-27 16:12:44 -0500

    better default op names, handle double param base item ops, add forceMethodOpName to meta, better parent detection and also add forceParent to meta, add extendBaseTo and forceItemPaths to meta. fixes digitalocean and github operation conflicts

--- openapi.go
+++ openapi.go
@@ -63,7 +63,12 @@ func (s *openapiService) Categories() []string {
 }
 
 func (s *openapiService) BaseURL() string {
-	return AsOrZero[string](s.schema.Get("servers", 0, "url"))
+	baseURL := AsOrZero[string](s.schema.Get("servers", 0, "url"))
+	baseExtension := s.meta.Get("extendBaseTo")
+	if !baseExtension.IsNil() {
+		baseURL, _ = url.JoinPath(baseURL, MustAs[string](baseExtension))
+	}
+	return baseURL
 }
 
 func (s *openapiService) DocsURL() string {
@@ -119,14 +124,15 @@ func (s *openapiService) Resources() []Resource {
 	if paths.IsNil() {
 		return nil
 	}
-
+	baseExtension := AsOrZero[string](s.meta.Get("extendBaseTo"))
 	relativePatterns := AsOrZero[[]string](s.meta.Get("relativeContentPaths"))
 
 	var res []*openapiResource
 	resLookup := make(map[string]*openapiResource)
 
-	for _, rawPath := range paths.Keys() {
-		resName := ToResourceName(rawPath)
+	for _, pathKey := range paths.Keys() {
+		fixedPath := strings.TrimPrefix(pathKey, baseExtension)
+		resName := ToResourceName(fixedPath)
 		if resName == "" {
 			continue
 		}
@@ -146,13 +152,13 @@ func (s *openapiService) Resources() []Resource {
 			orientation = "absolute"
 		}
 		for _, pattern := range relativePatterns {
-			if ok, _ := regexp.MatchString(pattern, rawPath); ok {
+			if ok, _ := regexp.MatchString(pattern, fixedPath); ok {
 				orientation = "relative"
 			}
 		}
 		p := &openapiPath{
-			parts:       strings.Split(rawPath, "/"),
-			schema:      paths.Get(rawPath),
+			parts:       strings.Split(fixedPath, "/"),
+			schema:      paths.Get(pathKey),
 			orientation: orientation,
 			resource:    r,
 		}
@@ -166,87 +172,13 @@ func (s *openapiService) Resources() []Resource {
 	return out
 }
 
-type openapiPath struct {
-	parts       []string
-	schema      *Value
-	orientation string
-
-	resource *openapiResource
-}
-
-func (p *openapiPath) name() string {
-	return strings.Join(p.parts, "/")
-}
-
-func (p *openapiPath) parentPathname() string {
-	return strings.Join(p.parts[:len(p.parts)-1], "/")
-}
-
-func (p *openapiPath) hasParamBase() bool {
-	lastSegment := p.parts[len(p.parts)-1]
-	return strings.HasPrefix(lastSegment, "{")
-}
-
-func (p *openapiPath) hasDoubleParamBase() bool {
-	if !p.hasParamBase() {
-		return false
-	}
-	if len(p.parts) < 2 {
-		return false
-	}
-	lastSegment := p.parts[len(p.parts)-2]
-	return strings.HasPrefix(lastSegment, "{")
-}
-
-func (p *openapiPath) isItemPath() bool {
-	return !p.isPluralBase()
-}
-
-func (p *openapiPath) isPluralBase() bool {
-	if p.hasParamBase() {
-		return false
-	}
-	lastSegment := p.parts[len(p.parts)-1]
-	// TODO: handle acronyms
-	return lastSegment == inflection.Plural(lastSegment)
-}
-
-func (p *openapiPath) sharedParams() (params []Schema) {
-	paramsRaw := p.schema.Get("parameters")
-	if paramsRaw.IsNil() {
-		return nil
-	}
-	for _, paramSchema := range paramsRaw.Items() {
-		param := &openapiParameter{
-			schema: paramSchema,
-		}
-		if param.ReadOnly() {
-			continue
-		}
-		params = append(params, param)
-	}
-	return
-}
-
-func (p *openapiPath) operations() (ops []*openapiOperation) {
-	for _, method := range p.schema.Keys() {
-		if strings.HasPrefix(method, "x-") || method == "parameters" {
-			continue
-		}
-		ops = append(ops, &openapiOperation{
-			path:   p,
-			method: method,
-			schema: p.schema.Get(method),
-		})
-	}
-	return
-}
-
 type openapiResource struct {
 	name    string
-	parent  *openapiResource
 	service *openapiService
 	paths   []*openapiPath
+
+	cachedParent    Resource
+	useCachedParent bool
 }
 
 func (r *openapiResource) Debug() string {
@@ -267,6 +199,29 @@ func (r *openapiResource) Subresources() (res []Resource) {
 }
 
 func (r *openapiResource) Parent() Resource {
+	if r.useCachedParent {
+		return r.cachedParent
+	}
+
+	defer func() {
+		r.useCachedParent = true
+	}()
+
+	reparent := r.service.meta.Get("forceParent", r.Name())
+	if !reparent.IsNil() {
+		parentName := AsOrZero[string](reparent)
+		if parentName == "" {
+			return nil
+		}
+		rr, err := r.service.Resource(parentName)
+		if err != nil {
+			log.Printf("!! unable to reparent '%s': %w\n", r.Name(), err)
+		} else {
+			r.cachedParent = rr
+			return rr
+		}
+	}
+
 	var shortestPath *openapiPath
 	if r.Orientation() == "relative" {
 		for _, p := range r.relativePaths() {
@@ -280,7 +235,8 @@ func (r *openapiResource) Parent() Resource {
 		}
 	}
 
-	findResourceByItemPath := func(pathname string) Resource {
+	pathname := shortestPath.parentPathname()
+	for pathname != "/" && pathname != "." && pathname != "" {
 		parentURL := denamePathParams(r.expandToURL(pathname))
 		for _, rr := range r.service.Resources() {
 			if rr.Name() == r.Name() {
@@ -288,26 +244,32 @@ func (r *openapiResource) Parent() Resource {
 			}
 			for _, u := range rr.ItemURLs() {
 				if denamePathParams(u) == parentURL {
+					r.cachedParent = rr
 					return rr
 				}
 			}
+			for _, u := range rr.CollectionURLs() {
+				if denamePathParams(u) == parentURL {
+					r.cachedParent = rr
+					return rr
+				}
+			}
+			if len(rr.CollectionURLs()) == 0 {
+				// even if collection endpoint doesn't exist, could be
+				// under what it might be if it did exist. though we
+				// need to compare paths not URLs to use path.Dir
+				for _, u := range rr.ItemURLs() {
+					iu, _ := url.Parse(u)
+					pu, _ := url.Parse(parentURL)
+					iu.Path = path.Dir(iu.Path)
+					if denamePathParams(iu.Path) == pu.Path {
+						r.cachedParent = rr
+						return rr
+					}
+				}
+			}
 		}
-		return nil
-	}
-
-	parentPathname := shortestPath.parentPathname()
-	parent := findResourceByItemPath(parentPathname)
-	if parent != nil {
-		return parent
-	}
-
-	// not found? try parent of parent path if item path
-	if shortestPath.isItemPath() {
-		parentPathname = path.Dir(parentPathname)
-	}
-	parent = findResourceByItemPath(parentPathname)
-	if parent != nil {
-		return parent
+		pathname = path.Dir(pathname)
 	}
 
 	return nil
@@ -423,26 +385,161 @@ func (r *openapiResource) Operation(name string) (Operation, error) {
 }
 
 func (r *openapiResource) Operations() (ops []Operation) {
+	names := make(map[string]*openapiOperation)
 	for _, p := range r.paths {
 		for _, o := range p.operations() {
 			ops = append(ops, o)
+			names[o.Name()] = o
 		}
 	}
-	names := make(map[string]bool)
+
+	names = make(map[string]*openapiOperation)
 	for _, op := range ops {
 		_, exists := names[op.Name()]
 		if exists {
 			log.Printf("!! multiple operations named '%s' on %s\n", op.Name(), r.name)
 		}
-		names[op.Name()] = true
+		names[op.Name()] = op.(*openapiOperation)
+	}
+	return
+}
+
+type openapiPath struct {
+	parts       []string
+	schema      *Value
+	orientation string
+
+	resource       *openapiResource
+	cachedItemPath *bool
+}
+
+func (p *openapiPath) name() string {
+	return strings.Join(p.parts, "/")
+}
+
+func (p *openapiPath) parentPathname() string {
+	return strings.Join(p.parts[:len(p.parts)-1], "/")
+}
+
+func (p *openapiPath) hasParamBase() bool {
+	lastSegment := p.parts[len(p.parts)-1]
+	return strings.HasPrefix(lastSegment, "{")
+}
+
+func (p *openapiPath) baseParamName() string {
+	lastSegment := p.parts[len(p.parts)-1]
+	if !strings.HasPrefix(lastSegment, "{") {
+		return ""
+	}
+	return strings.Trim(lastSegment, "{}")
+}
+
+func (p *openapiPath) hasDoubleParamBase() bool {
+	if !p.hasParamBase() {
+		return false
+	}
+	if len(p.parts) < 2 {
+		return false
+	}
+	lastSegment := p.parts[len(p.parts)-2]
+	return strings.HasPrefix(lastSegment, "{")
+}
+
+func (p *openapiPath) isItemPath() (b bool) {
+	if p.cachedItemPath != nil {
+		return *p.cachedItemPath
+	}
+
+	defer func() {
+		p.cachedItemPath = &b
+	}()
+
+	forcedItem := p.resource.service.meta.Get("forceItemPaths", p.name())
+	if !forcedItem.IsNil() {
+		b = MustAs[bool](forcedItem)
+		return
+	}
+
+	// /path/{} or /path/{}/{} is prob item
+	if p.hasParamBase() || p.hasDoubleParamBase() {
+		b = true
+		return
+	}
+
+	// if /path when /path/{} exists, prob not item
+	if p.isDirOfItemPath() {
+		b = false
+		return
+	}
+
+	// otherwise, if base segment is plural, prob not item
+	b = !p.isPluralBase()
+	return
+}
+
+func (p *openapiPath) isDirOfItemPath() bool {
+	for _, pp := range p.siblingPaths() {
+		if pp.parentPathname() == p.name() {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *openapiPath) isPluralBase() bool {
+	if p.hasParamBase() {
+		return false
+	}
+	lastSegment := p.parts[len(p.parts)-1]
+	// TODO: handle acronyms
+	return lastSegment == inflection.Plural(lastSegment)
+}
+
+func (p *openapiPath) sharedParams() (params []Schema) {
+	paramsRaw := p.schema.Get("parameters")
+	if paramsRaw.IsNil() {
+		return nil
+	}
+	for _, paramSchema := range paramsRaw.Items() {
+		param := &openapiParameter{
+			schema: paramSchema,
+		}
+		if param.ReadOnly() {
+			continue
+		}
+		params = append(params, param)
+	}
+	return
+}
+
+func (p *openapiPath) siblingPaths() (paths []*openapiPath) {
+	for _, pp := range p.resource.paths {
+		if pp != p {
+			paths = append(paths, pp)
+		}
+	}
+	return
+}
+
+func (p *openapiPath) operations() (ops []*openapiOperation) {
+	for _, method := range p.schema.Keys() {
+		if strings.HasPrefix(method, "x-") || method == "parameters" {
+			continue
+		}
+		ops = append(ops, &openapiOperation{
+			path:   p,
+			method: method,
+			schema: p.schema.Get(method),
+		})
 	}
 	return
 }
 
 type openapiOperation struct {
-	path   *openapiPath
-	method string
-	schema *Value
+	path         *openapiPath
+	method       string
+	schema       *Value
+	nameOverride string
 }
 
 func (o *openapiOperation) Resource() Resource {
@@ -458,18 +555,39 @@ func (o *openapiOperation) Name() string {
 }
 
 func (o *openapiOperation) AbsName() string {
+	forceNames := o.path.resource.service.meta.Get("forceMethodOpName")
+	if !forceNames.IsNil() {
+		name := forceNames.Get(o.path.name(), o.method)
+		if !name.IsNil() {
+			return MustAs[string](name)
+		}
+	}
+
+	if o.nameOverride != "" {
+		return o.nameOverride
+	}
 	mapping := map[string]string{
 		"get":    "list",
-		"put":    "set",
-		"patch":  "update",
-		"delete": "delete",
+		"put":    "replace",
+		"patch":  "patch",
+		"delete": "purge",
 		"post":   "create",
 		"head":   "scan",
 	}
 	if o.path.isItemPath() {
-		mapping["get"] = "get"
-		mapping["head"] = "check"
-		mapping["post"] = "post" // TODO: something better
+		mapping = map[string]string{
+			"get":    "get",
+			"put":    "set",
+			"patch":  "update",
+			"delete": "delete",
+			"post":   "apply",
+			"head":   "check",
+		}
+		if o.path.hasDoubleParamBase() {
+			// if /path/{}/{} then /path/{} prob exists and we need to
+			// differentiate, so we use the final path param name
+			mapping["get"] = toCamelCase(fmt.Sprintf("get_with_%s", o.path.baseParamName()))
+		}
 	}
 	return cmp.Or(mapping[o.method], "??"+o.method)
 }
